083d95cd15c063dbafc27c220886f6cd62591473
LAPTOP-A9CDTQD3\ALFALAH <sramadhan95@gmail.com>
2020-10-02 06:04:31 +0700

    perbaikan path css dan js pada file template

--- app/Views/layout/template.php
+++ app/Views/layout/template.php
@@ -11,7 +11,7 @@
     <link rel="canonical" href="https://getbootstrap.com/docs/4.5/examples/offcanvas/">
 
     <!-- Bootstrap core CSS -->
-    <link href="/assets/dist/css/bootstrap.css" rel="stylesheet">
+    <link href="<?= base_url('/assets/dist/css/bootstrap.css'); ?>" rel="stylesheet">
 
     <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -41,8 +41,8 @@
       }
     </style>
     <!-- Custom styles for this template -->
-    <link href="/css/offcanvas.css" rel="stylesheet">
-    <link href="/css/style.css" rel="stylesheet">
+    <link href="<?= base_url('/css/offcanvas.css') ?>" rel="stylesheet">
+    <link href="<?= base_url('/css/style.css'); ?>" rel="stylesheet">
   </head>
   <body class="bg-light">
     
@@ -61,8 +61,8 @@
     <!--<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>-->
     <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-    <script>window.jQuery || document.write('<script src="/assets/js/vendor/jquery.slim.min.js"><\/script>')</script><script src="/assets/dist/js/bootstrap.bundle.js"></script>
-    <script src="/js/offcanvas.js"></script>
+    <script>window.jQuery || document.write('<script src="<?= base_url('/assets/js/vendor/jquery.slim.min.js'); ?>"><\/script>')</script><script src="<?= base_url('/assets/dist/js/bootstrap.bundle.js'); ?>"></script>
+    <script src="<?= base_url('/js/offcanvas.js') ?>"></script>
     <script>
         $(document).ready(function() {
           $("#tanggal_pelaksanaan").datepicker();
