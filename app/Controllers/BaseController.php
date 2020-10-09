18e2119114d6a692d010ba331aff5aca57c159bb
LAPTOP-A9CDTQD3\ALFALAH <sramadhan95@gmail.com>
2020-10-09 11:26:53 +0700

    membuat singkat label bulan

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -89,40 +89,40 @@ class BaseController extends Controller
 	function bulan($bln){
 		switch ($bln){
 			case 1:
-				return "Januari";
+				return "Jan";
 				break;
 			case 2:
-				return "Februari";
+				return "Feb";
 				break;
 			case 3:
-				return "Maret";
+				return "Mar";
 				break;
 			case 4:
-				return "April";
+				return "Apr";
 				break;
 			case 5:
 				return "Mei";
 				break;
 			case 6:
-				return "Juni";
+				return "Jun";
 				break;
 			case 7:
-				return "Juli";
+				return "Jul";
 				break;
 			case 8:
-				return "Agustus";
+				return "Agu";
 				break;
 			case 9:
-				return "September";
+				return "Sep";
 				break;
 			case 10:
-				return "Oktober";
+				return "Okt";
 				break;
 			case 11:
-				return "November";
+				return "Nov";
 				break;
 			case 12:
-				return "Desember";
+				return "Des";
 				break;
 		}
 	}
