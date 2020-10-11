96bde740d385e853a5fea0a5681b03895a3847d6
LAPTOP-A9CDTQD3\ALFALAH <sramadhan95@gmail.com>
2020-10-11 20:54:26 +0700

    perubahan filter jenis klpd pada halaman detail jenis advokasi

--- app/Controllers/JenisAdvokasi.php
+++ app/Controllers/JenisAdvokasi.php
@@ -59,7 +59,7 @@ class JenisAdvokasi extends BaseController
 			'jenis_klpd' => $jenis_klpd,
 			'tahun' => $tahun,
 			'id' => $id,
-			'options_jenis_klpd' => $this->options_jenis_klpd(),
+			'options_jenis_klpd' => $this->options_jenis_klpd_extra(),
 			'options_tahun_layanan' => $this->options_tahun_layanan()
 		];
 
