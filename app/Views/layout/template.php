9c04e6ad68bd539fc4f6654ebbc43d842210316b
LAPTOP-A9CDTQD3\ALFALAH <sramadhan95@gmail.com>
2020-11-04 13:01:47 +0700

    add js dan css pada lokal penyimpanan

--- app/Views/layout/template.php
+++ app/Views/layout/template.php
@@ -12,9 +12,9 @@
     <!-- Bootstrap core CSS -->
     <link href="<?= base_url('/assets/dist/css/bootstrap.css'); ?>" rel="stylesheet">
 
-    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/css/select2.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="<?= base_url('css/jquery-ui.css'); ?>">
+    <link rel="stylesheet" href="<?= base_url('fontawesome-5.15.1/css/font-awesome.min.css');?>">
+    <link href="<?= base_url('css/select2.min.css'); ?>" rel="stylesheet" />
     <style>
       .bd-placeholder-img {
         font-size: 1.125rem;
@@ -70,11 +70,11 @@
     </div>
 
     <!--<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>-->
-    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/select2.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.15/jquery.mask.min.js"></script>
-    <script>window.jQuery || document.write('<script src="<?= base_url('/assets/js/vendor/jquery.slim.min.js'); ?>"><\/script>')</script><script src="<?= base_url('/assets/dist/js/bootstrap.bundle.js'); ?>"></script>
+    <script src="<?= base_url('js/jquery-1.12.4.js'); ?>"></script>
+    <script src="<?= base_url('js/select2.min.js'); ?>"></script>
+    <script src="<?= base_url('js/jquery.mask.min.js'); ?>"></script>
+    <script>window.jQuery || document.write('<script src="<?= base_url('/assets/js/vendor/jquery.slim.min.js'); ?>"><\/script>')</script>
+    <script src="<?= base_url('/assets/dist/js/bootstrap.bundle.js'); ?>"></script>
     <script src="<?= base_url('/js/offcanvas.js') ?>"></script>
     <script>
         $(document).ready(function() {
